d5e3874e5a294078451ea49f258abeea5e93e54d
foacus <hanglandtroy@gmail.com>
2025-02-18 12:06:06 -0500

    Add tip file

---
+++ week-05/tip-calculator-master/README.md
@@ -0,0 +1,18 @@
+# Tip Calculator
+An exercise in JavaScript function definitions and return values. Course: DMD 3475. 
+
+## Introduction
+Our tip calculator application is almost complete. Our function named `updateValues()` will fetch the values that were input by the user. 
+
+At that point, the values are passed as arguments to two functions named `calculateTipPerPerson()` and `calculateTotalPerPerson()`. The values that are returned by each function are used to update the DOM.
+
+## Instructions
+
+1. Define both required functions: `calculateTipPerPerson()` and `calculateTotalPerPerson()`.
+2. Each function should accept the 3 arguments being passed to them within the `updateValues()` function.
+3. Using those values, calculate and return the appropriate answer.
+4. Extra credit: Properly round the dollars and cents that are returned by your functions.
+
+## Helpful Information
+* Search Google for "Tip Calculator" to see the desired functionality for this application.
+* The `calculateTotalPerPerson()` function should return a value that includes the tip.
---
+++ week-05/tip-calculator-master/js/script.js
@@ -0,0 +1,52 @@
+/**
+ * Get references to each of the form inputs
+ */
+let total = document.querySelector('#total');
+let tip = document.querySelector('#tip');
+let people = document.querySelector('#people');
+
+/**
+ * Whenever the fields are updated, we call
+ * the updateValues function. 
+ */
+total.addEventListener('change', updateValues);
+total.addEventListener('keyup', updateValues);
+tip.addEventListener('change', updateValues);
+tip.addEventListener('keyup', updateValues);
+people.addEventListener('change', updateValues);
+people.addEventListener('keyup', updateValues);
+
+/**
+ * We will need references for the span tags
+ * that we will use to output our results.
+ */
+let tipOutput = document.querySelector(".tipOutput");
+let totalOutput = document.querySelector(".totalOutput");
+
+/**
+ * This function is run every time an input is changed
+ * by a user.
+ */
+function updateValues(event){
+
+    // Get the latest values from our form inputs
+    let totalValue = total.value;
+    let tipValue = tip.value;
+    let peopleValue = people.value;
+
+    // Update the values on the screen
+    tipOutput.innerText = calculateTipPerPerson(totalValue, tipValue, peopleValue);
+    totalOutput.innerText = calculateTotalPerPerson(totalValue, tipValue, peopleValue);
+}
+
+/**
+ * Your code goes down here ...
+ * @todo Write functions for calculateTipPerPerson() and calculateTotalPerPerson()
+ */
+
+function calculateTipPerPerson(totalValue, tipValue, peopleValue){
+
+}
+function calculateTotalPerPerson(totalValue, tipValue, peopleValue){
+    
+}
