f70f1451c554ae257dd930ce1cf66c1914688053
foacus <164416831+foacus@users.noreply.github.com>
2025-03-08 01:39:13 -0500

    finished tutorial thingy

--- week-07/js/week7_webpage.js
+++ week-07/js/week7_webpage.js
@@ -1,2 +1,35 @@
-document.body.style.backgroundColor = "lightblue";
-alert("your computer has been hacked - this is from the new js file");
+const person1 = {
+    name: ["Troy", "Hangland"],
+    age: 21,
+    bio() {
+        console.log(`${this.name[0]} ${this.name[1]} is ${this.age} years old.`);
+    },
+    introduceSelf: function () {
+        console.log(`Hi! I am ${this.name[0]}.`);
+    },
+    jump() {
+        console.log(`${this.name[0]} can jump ${Math.floor(Math.random() * 10)} feet high!`)
+    },
+};
+
+const person2 = {
+    name: ["Katie", "Gomes"],
+    age: 20,
+    bio() {
+        console.log(`${this.name[0]} ${this.name[1]} is ${this.age} years old.`);
+    },
+    introduceSelf: function () {
+        console.log(`Hi! I am ${this.name[0]}.`);
+    },
+    jump() {
+        console.log(`${this.name[0]} can jump ${Math.floor(Math.random() * 10)} feet high!`)
+    },
+};
+
+//function Person (name) {
+  //  this.name = name;
+    //this.introduceSelf = function () {
+    //  console.log(`Hi! I'm ${this.name}.`);
+   // };
+  //}
+  
