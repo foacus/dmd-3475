417b03fc699cd09440756fe37c0aed7fea62f281
foacus <hanglandtroy@gmail.com>
2025-03-13 15:37:18 -0400

    a

---
+++ week-08/dom-playground-master/README.md
@@ -0,0 +1,28 @@
+# DOM Playground
+
+## Instructions
+Fix the page design and layout by modifying the DOM. When complete, your page should look like the original, "[Wicked Grove](http://www.csszengarden.com/006/)" design.
+
+You'll need to look for the comments found in the `index.html` file. Follow the instructions in each comment, but do not modify the HTML or CSS file. All modifications must be done in JavaScript. Use the `js/fix-the-page.js` file for your DOM modifications. 
+
+## Rules
+1) You may not modify the HTML file.
+2) You may not modify the CSS file.
+
+You can use the original HTML and CSS files for reference.
+
+## What You Need to Know
+This assignment is designed to test your knowledge of the following JavaScript features:
+
+* `document.querySelector()`
+* `document.createElement()`
+* `element.appendChild()`
+* `element.innerHTML`
+* `element.style`
+* `element.attribute`
+* `element.classList.add()`
+* `Array.forEach()`
+* The DOM
+* Object Literals `{}`
+* ... and more!
+
---
+++ week-08/dom-playground-master/js/fix-the-page.js
@@ -0,0 +1,50 @@
+/**
+ * Use this javascript file to fix the page design and layout. 
+ * Reference the index.html comments for complete details.
+ */
+
+// 1) Add CSS Properties to the body tag
+
+
+// 2) Add the .page-wrapper class
+
+
+// 3) Add the .summary class
+
+
+// 4) Add the .sidebar class
+
+
+// 5) Add these links to the "Resources" <ul>
+let resources = [
+    {
+        title: "View the source CSS file of the currently-viewed design.",
+        href: "css/style.css",
+        linkText: "View This Design&#8217;s <abbr title=\"Cascading Style Sheets\">CSS</abbr>"
+    },
+    {
+        title: "Links to great sites with information on using CSS.",
+        href: "http://www.mezzoblue.com/zengarden/resources/",
+        linkText: "<abbr title=\"Cascading Style Sheets\">CSS</abbr> Resources "
+    },
+    {
+        title: "A list of Frequently Asked Questions about the Zen Garden.",
+        href: "http://www.mezzoblue.com/zengarden/faq/",
+        linkText: "<abbr title=\"Frequently Asked Questions\">FAQ</abbr> "
+    },   
+    {
+        title: "Send in your own CSS file.",
+        href: "http://www.mezzoblue.com/zengarden/submit/",
+        linkText: "Submit a Design"
+    },
+    {
+        title: "View translated versions of this page.",
+        href: "http://www.mezzoblue.com/zengarden/translations/",
+        linkText: "Translations"
+    },
+];
+
+// Hint: Loop over the resources array to build each list item. 
+// E.g., <li><a href="someLink" title="someTitle">someText</a></li>
+
+
