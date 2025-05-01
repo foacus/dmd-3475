62258d6861b1f389fb8de4dd9e3818cee5820d3e
foacus <164416831+foacus@users.noreply.github.com>
2025-04-30 23:47:30 -0400

    added badge and interactivivty

--- final-project/js/main.js
+++ final-project/js/main.js
@@ -1,36 +1,45 @@
-AFRAME.registerComponent("hover-info", {
-    schema: {
-      panel: { type: "selector" }
-    },
-  
+AFRAME.registerComponent("revolver", {
     init: function () {
-      const el = this.el;
-      const panel = this.data.panel;
-  
-      el.addEventListener("mouseenter", () => {
-        // Move object up
-        el.setAttribute("animation__up", {
-          property: "position",
-          to: `${el.object3D.position.x} ${el.object3D.position.y + 0.3} ${el.object3D.position.z}`,
-          dur: 200,
-          easing: "easeOutQuad"
-        });
-  
-        // Show panel
-        if (panel) panel.setAttribute("visible", true);
-      });
-  
-      el.addEventListener("mouseleave", () => {
-        // Move object back down
-        el.setAttribute("animation__down", {
-          property: "position",
-          to: `${el.object3D.position.x} ${el.object3D.position.y - 0.3} ${el.object3D.position.z}`,
-          dur: 200,
-          easing: "easeInQuad"
-        });
-  
-        // Hide panel
-        if (panel) panel.setAttribute("visible", false);
-      });
+      this.isUp = false;
+    },
+    events: {
+      click: function (e) {
+          const el = this.el;
+          const panel = document.querySelector('#revovlerInfoText');
+          panel.setAttribute("visible", !panel.getAttribute("visible"));
+
+          if(this.isUp == false){
+            this.isUp = true;
+            el.setAttribute("position","-0.647 1 -2.567")
+            el.setAttribute("rotation","9.215 -69.728 -2.567")
+        } else {
+            this.isUp = false;
+            el.setAttribute("position","-0.647 0.696 -2.567")
+            el.setAttribute("rotation","-0.269 -47.830 90.698")
+        }
+      },
+    }
+  });
+
+  AFRAME.registerComponent("badge", {
+    init: function () {
+      this.isUp = false;
+    },
+    events: {
+      click: function (e) {
+          const el = this.el;
+          const panel = document.querySelector('#badgeInfoText');
+          panel.setAttribute("visible", !panel.getAttribute("visible"));
+
+          if(this.isUp == false){
+            this.isUp = true;
+            el.setAttribute("position","-0.050 1.199 -2.685")
+            el.setAttribute("rotation","53.633 -34.346 -30.063")
+        } else {
+            this.isUp = false;
+            el.setAttribute("position","-0.050 0.687 -2.685")
+            el.setAttribute("rotation","2.908 -19.571 -2.275")
+        }
+      },
     }
   });
